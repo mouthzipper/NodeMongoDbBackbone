05a803efc4a29e04e88c3dd3f997de460a06087c
jerome-ramos <ramos.jeromeh@gmail.com>
2013-09-26 00:31:27 +0800

    Initial Commit

---
+++ public/js/views/forgotpassword.js
@@ -0,0 +1,28 @@
+define(['TambayanView', 'text!templates/forgotpassword.html'], function(TambayanView, forgotpasswordTemplate) {
+  var forgotpasswordView = TambayanView.extend({
+    requireLogin: false,
+
+    el: $('#content'),
+
+    events: {
+      "submit form": "password"
+    },
+
+    password: function() {
+       window.location.hash = 'login';
+      $.post('/forgotpassword', {
+        email: $('input[name=email]').val()
+      }, function(data) {
+        console.log(data);
+
+      });
+      return false;
+    },
+
+    render: function() {
+      this.$el.html(forgotpasswordTemplate);
+    }
+  });
+
+  return forgotpasswordView;
+});
---
+++ public/js/views/logout.js
@@ -0,0 +1,28 @@
+define(['TambayanView', 'text!templates/register.html'], function(TambayanView, registerTemplate) {
+  var registerView = TambayanView.extend({
+    requireLogin: false,
+
+	el: $('#content'),
+
+    events: {
+      "submit form": "logout"
+    },
+
+    logout: function() {
+      $('#profile').hide();
+        window.activeSession.clear();
+         window.location.hash = 'login';
+     
+    },
+
+    render: function() {
+      this.$el.html(registerTemplate);
+       $("#error").hide();
+       $("#success").hide();
+      $("input[name=firstName]").focus();
+    }
+  });
+
+  return registerView;
+});
+
